1ee6e79b395c3fa314fef1380866c9d007788db8
gibin thomson <gibinthom@gmail.com>
2024-12-27 00:43:14 +0530

    api call to get weather forcast for next 5 datys

--- backend/src/routes/weather.route.ts
+++ backend/src/routes/weather.route.ts
@@ -1,19 +1,18 @@
-import express from 'express';
-import { sendError, sendResponse } from '../shared/server-response';
-import * as weatherService from '../services/weather.service';
+import express from "express";
+import { sendError, sendResponse } from "../shared/server-response";
+import * as weatherService from "../services/weather.service";
 
 const router = express.Router();
 
-router.get('/forecast', async function (req, res) {
-    let lat = Number(req.query.lat);
-    let lng = Number(req.query.lng);
-
-    let response
-    try {
-        response = await weatherService.getWeatherForecast(lat, lng);
-    } catch (e) {
-        return sendError(res, e);
-    }
+router.get("/forecast", async function (req, res) {
+  let lat = Number(req.query.lat);
+  let lng = Number(req.query.lng);
+  let response;
+  try {
+    response = await weatherService.getWeatherForecast(lat, lng);
+  } catch (e) {
+    return sendError(res, e);
+  }
 
   return sendResponse(res, response);
 });
--- backend/src/services/weather.service.ts
+++ backend/src/services/weather.service.ts
@@ -1,34 +1,41 @@
+import axios from "axios";
 
 export interface ForecastModel {
-    latitude: number,
-    longitude: number,
-    elevation: number,
-    current_weather: {
-        temperature: number,
-        windspeed: number,
-        winddirection: number,
-        weathercode: number,
-        time: Date
-    }
+  latitude: number;
+  longitude: number;
+  daily_units: {
+    temperature_2m_max: string;
+    temperature_2m_min: string;
+  };
+  daily: {
+    time: string[];
+    temperature_2m_max: number[];
+    temperature_2m_min: number[];
+  };
+}
+export interface Response {
+  status: string;
+  data: ForecastModel | {};
 }
 
-
-export async function getWeatherForecast(lat: number, lng: number): Promise<ForecastModel> {
-
-    // TODO: make a call to this API with latitude and longitude from the frontend
-    // use axios to make the call
-    //  https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true
-
-    return {
-        latitude: 52.52,
-        longitude: 13.41,
-        elevation: 0,
-        current_weather: {
-            temperature: 10,
-            windspeed: 10,
-            winddirection: 10,
-            weathercode: 10,
-            time: new Date()
-        }
+export async function getWeatherForecast(
+  lat: number,
+  lng: number
+): Promise<any> {
+  let response: any = {};
+  try {
+    const res = await axios.get(
+      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset`
+    );
+    response = {
+      status: "ok",
+      data: res.data,
     };
+  } catch (error: any) {
+    response = {
+      status: "nok",
+      data: { error: error.message || "An error occurred" },
+    };
+  }
+  return { result: response };
 }
